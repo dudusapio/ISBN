5ee8137baeb1444ea22dac3e3fd5240d8669aa1a
eduardo sapio <dudusapio@live.com>
2021-08-28 22:55:04 -0300

    Quase-1

--- atividade5.c
+++ atividade5.c
@@ -5,7 +5,7 @@
 int isbn_dv(char isbn[10]){
     int v[10],s1[10],s2[10],verificador,i,x;
     int  num = atoi(isbn);
-    printf("num = %d",num);
+    //printf("num = %d",num);
     
     v[0] = num / 1 % 10;
     v[1] = num / 10 % 10;
@@ -16,34 +16,31 @@ int isbn_dv(char isbn[10]){
     v[6] = num / 1000000 % 10;
     v[7] = num / 10000000 % 10;
     v[8] = num / 100000000 % 10;
-    v[9] = num / 1000000000 % 10;
 
-    for( i = 0; i < 10 ; i ++){
+    for( i = 0; i < 9 ; i ++){
     printf("v[%d] = %d\n",i,v[i]);
     }
-    x = 8;
+    x = 7;
     
     s1[0] = 0;
-    for( i = 1; i < 10; i++){
+    for( i = 1; i < 9; i++){
         s1[i] = s1[i - 1] + v[x];
         x--;
     }
-    for( i = 0; i < 10; i++){
+    for( i = 0; i < 9; i++){
         printf("s1[%d] = %d\n",i,s1[i]);
+    }
+    s2[0] = 0;
+    for( i = 1; i < 9; i ++){
+        s2[i] = s2[i - 1] + s1[i];
+    }
+    for( i = 0; i < 9; i++){
+        printf("s2[%d] = %d\n",i,s2[i]);
     }
 }
 int main(void){
     char x[15];
-    strcpy(x,"0123456789");
+    strcpy(x,"089237010");
     isbn_dv(x);
 }
 
-// int main(void){
-//     int a = 102;
-//    int  b = a / 1 % 10;
-//    int c = a / 10 % 10;
-//    int d = a / 100 % 10;
-//     printf("b = %d",b);
-//     printf("c = %d",c);
-//     printf("d = %d",d);
-// }
